5f6831de6c3f3ddde71daaef5bfa39b078a5abba
thirumurugan <thiru123arul@outlook.com>
2021-08-01 19:52:42 +0530

    app icon dark mode and animation on font container

--- static/js/home.js
+++ static/js/home.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     $('.slide-menu .slide-menu-item li').click(clickedForRipple);
     $('.dropbtn').click(clickedForDropdown)
     
-    //clicked for font
-    $('.fonts-cnt .font').click(clickedForRipple)
+    //clicked for font moved to home page api after inserting element
+    //$('.fonts-cnt .font').click(clickedForRippleOnFont)
 
     //toggle of dark-light mode
     $('#dark-light-toggle').change(toggleDarkLight)
@@ -509,6 +509,50 @@ function clickedForRipple(e) {
 
     // remove all active
     $(this).parent().children().removeClass('active')
+
+    // add active class
+    $(this).addClass('active')
+
+    $('')
+    // Setup
+    var posX = $(this).offset().left,
+        posY = $(this).offset().top,
+        buttonWidth = $(this).width(),
+        buttonHeight = $(this).height();
+
+    // Add the element
+    $(this).prepend("<span class='ripple'></span>");
+
+    // Make it round!
+    if (buttonWidth >= buttonHeight) {
+        buttonHeight = buttonWidth;
+    } else {
+        buttonWidth = buttonHeight;
+    }
+
+    // Get the center of the element
+    var x = e.pageX - posX - buttonWidth / 2;
+    var y = e.pageY - posY - buttonHeight / 2;
+
+
+    // Add the ripples CSS and start the animation
+    $(".ripple").css({
+        width: buttonWidth,
+        height: buttonHeight,
+        top: y + 'px',
+        left: x + 'px'
+    }).addClass("rippleEffect");
+}
+
+// for font container
+function clickedForRippleOnFont(e) {
+    console.log('come here')
+
+    // Remove any old one   
+    $(".ripple").remove();
+
+    // remove all active
+    $(".fonts-cnt .font.active").removeClass('active')
 
     // add active class
     $(this).addClass('active')
