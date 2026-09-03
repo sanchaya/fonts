6a28d58a5d7f90868ec39eb65f05d914040c5889
Omshivaprakash H L <omshivaprakash@gmail.com>
2026-09-03 10:00:12 +0530

    fix: support bar visibility, blue outline on click, font card CSS quoting
    - Fixed support bar not showing: removed duplicate CSS that hid it with visibility/opacity
    - Fixed blue outline on click: added *:focus { outline: none }
    - Fixed font cards showing one font: fixed broken CSS quoting in font-family attribute

--- static/js/home-api.js
+++ static/js/home-api.js
@@ -150,7 +150,7 @@ var appendCard = (res) => {
             qualityBadge = '<span class="quality-badge-card" style="background:'+res.data[i].quality.gradeColor+';">'+res.data[i].quality.grade+'</span>'+errBadge+wrnBadge
         }
         
-        appender += '<a href="/family/'+res.data[i].link+'"><div class="font"><div class="header"><h4 class="family-name">'+res.data[i].family+'</h4>'+authorInfo+'<span class="'+licenseClass+'">'+licenseLabel+'</span>'+qualityBadge+'<span class="n-styles">'+res.data[i].styles+' styles</span></div><div class="font-text" style="font-family:"'+res.data[i].font+'", recursive;">Almost before we know it, we had left the ground</div></div></a>'
+        appender += '<a href="/family/'+res.data[i].link+'"><div class="font"><div class="header"><h4 class="family-name">'+res.data[i].family+'</h4>'+authorInfo+'<span class="'+licenseClass+'">'+licenseLabel+'</span>'+qualityBadge+'<span class="n-styles">'+res.data[i].styles+' styles</span></div><div class="font-text" style="font-family:\''+res.data[i].font+'\', recursive;">Almost before we know it, we had left the ground</div></div></a>'
     }
     if(!isLastPage)
         appender += "<div id='loading-gif'><img src='/img/loading.gif'/></div>"
